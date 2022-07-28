1714ccfa877c99054e9ef1f9e97249b10ba835d9
dan salahi <dan.salahii@gmail.com>
2022-07-28 11:30:57 +0300

    added nav slices for the redux

---
+++ Store/Slices/NavSlices.ts
@@ -0,0 +1,39 @@
+import {createSlice} from '@reduxjs/toolkit';
+import {RootState} from '../Store';
+// import type {PayloadAction} from '@reduxjs/toolkit';
+
+export interface CounterState {
+  value: number;
+}
+
+const initialState = {
+  origin: null,
+  destination: null,
+  travelTimeInformation: null,
+};
+
+export const navSlice = createSlice({
+  name: 'nav',
+  initialState,
+  reducers: {
+    setOrigin: (state, action) => {
+      state.origin = action.payload;
+    },
+    setDestination: (state, action) => {
+      state.destination = action.payload;
+    },
+    setTravelTimeInformation: (state, action) => {
+      state.travelTimeInformation = action.payload;
+    },
+  },
+});
+
+export const {setOrigin, setDestination, setTravelTimeInformation} =
+  navSlice.actions;
+
+export const selectOrigin = (state: RootState) => state.nav.origin;
+export const selectDestination = (state: RootState) => state.nav.destination;
+export const selectTravelTimeInformation = (state: RootState) =>
+  state.nav.travelTimeInformation;
+
+export default navSlice.reducer;
