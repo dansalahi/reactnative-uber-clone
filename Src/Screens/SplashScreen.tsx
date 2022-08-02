0ae0f50aad375935aa4539160e7bbc1c01839be0
dan salahi <dan.salahii@gmail.com>
2022-08-02 12:37:16 +0300

    added the splash screen

---
+++ Src/Screens/SplashScreen.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+const SplashScreen = () => {
+  return (
+    <View>
+      <Text>SplashScreen</Text>
+    </View>
+  );
+};
+
+export default SplashScreen;
