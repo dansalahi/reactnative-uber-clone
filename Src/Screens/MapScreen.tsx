f3a16e5f30da4970d784f190d57fc5d43d1356e1
daniel salahi <dan.salahii@gmail.com>
2022-09-14 08:45:51 +0300

    added the mapscreen

---
+++ Src/Screens/MapScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const MapScreen = () => {
+  return (
+    <View>
+      <Text>MapScreen</Text>
+    </View>
+  );
+};
+
+export default MapScreen;
