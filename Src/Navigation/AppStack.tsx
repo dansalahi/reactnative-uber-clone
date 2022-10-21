88aee5e4e7cd17e4142c70728ee7389f1fa41a25
daniel salahi <dan.salahii@gmail.com>
2022-10-21 09:33:46 +0300

    updated the app stack with map screen

--- Src/Navigation/AppStack.tsx
+++ Src/Navigation/AppStack.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from '../Screens/HomeScreen';
+import MapScreen from '../Screens/MapScreen';
 //
 
 export const AppStack = createNativeStackNavigator();
@@ -8,6 +9,7 @@ export const AppStackNavigation = () => {
   return (
     <AppStack.Navigator screenOptions={{headerShown: false}}>
       <AppStack.Screen name="HomeScreen" component={HomeScreen} />
+      <AppStack.Screen name="MapScreen" component={MapScreen} />
     </AppStack.Navigator>
   );
 };
