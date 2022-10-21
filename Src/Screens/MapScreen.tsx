f2fd3730afceffbcf1a953ccf82057d9512d4aa7
daniel salahi <dan.salahii@gmail.com>
2022-10-21 09:33:23 +0300

    added the map screen

--- Src/Screens/MapScreen.tsx
+++ Src/Screens/MapScreen.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Button, Text, View} from 'react-native';
+import Screen from '../Components/Theme/Screen';
+import tw from 'twrnc';
+import {useNavigation} from '@react-navigation/native';
 
 const MapScreen = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>MapScreen</Text>
-    </View>
+    <Screen style={tw`flex flex-row items-center justify-center`}>
+      <View>
+        <Text>MapScreen</Text>
+        <Button
+          title="back"
+          onPress={() => navigation.navigate('HomeScreen' as never)}
+        />
+      </View>
+    </Screen>
   );
 };
 
